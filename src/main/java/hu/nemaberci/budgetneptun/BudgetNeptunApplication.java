453953ca71a46c80b5c830206e851eb3a7067ead
bertalan.kovacs <bertalan.kovacs@ivm-vending.com>
2022-05-15 14:16:09 +0200

    initial commit

---
+++ src/main/java/hu/nemaberci/budgetneptun/BudgetNeptunApplication.java
@@ -0,0 +1,13 @@
+package hu.nemaberci.budgetneptun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetNeptunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetNeptunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/nemaberci/budgetneptun/BudgetNeptunApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nemaberci.budgetneptun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetNeptunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
